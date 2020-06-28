6aa0c2f29de9a07a62056ac701d0049ae82f927c
Shalin Amir Ali <sa06132@st.habib.edu.pk>
2020-06-28 16:37:06 +0500

    Graph values updated for PSO_validation

--- Code/PySwarm_validation_rastrigin.py
+++ Code/PySwarm_validation_rastrigin.py
@@ -1,57 +1,83 @@
 # Import modules
-# import matplotlib.pyplot as plt
+
 import numpy as np
-# from IPython.display import Image
+
 # Import PySwarms
 import pyswarms as ps
 from pyswarms.utils.functions import single_obj as fx
-# from pyswarms.utils.plotters import (plot_cost_history, plot_contour, plot_surface)
+import time
 
-import csv
+# setting hyperparameters
+max_bound = 5.12 * np.ones(2)
+# print(max_bound)
+min_bound = - max_bound
+# print(min_bound)
+bounds = (min_bound, max_bound)
+velocity_clamp = (0, 15)
+options = {'c1': 0.5, 'c2': 0.3, 'w': 0.9}
+# calling PSO
+optimizer = ps.single.GlobalBestPSO(n_particles=20, dimensions=2, options=options, bounds=bounds, velocity_clamp=velocity_clamp)
+# optimizing
+optimizer.optimize(fx.rastrigin, 50)
 
-# Set-up hyperparameters
-
-# Call instance of PSO
-
-
-def LoadData(filename):
-    with open(filename) as csvfile:
-        readCSV = csv.reader(csvfile, delimiter=",")
-        max_bound = 5.12 * np.ones(2)
-        # print(max_bound)
-        min_bound = - max_bound
-        # print(min_bound)
-        bounds = (min_bound, max_bound)
-        velocity_clamp = (0, 15)
-
-        options = {'c1': 0.5, 'c2': 0.3, 'w': 0.9}
-        for i in readCSV:
-            variable = i[0]
-            print('--------------------Testing on different',
-                  variable + '------------------')
-            i = i[1:]
-            for n in i:
-                if variable == 'Num_Iterations':
-                    # optimizes function on different numver of iterations
-                    optimizer = ps.single.GlobalBestPSO(
-                        n_particles=20, dimensions=2, options=options, bounds=bounds, velocity_clamp=velocity_clamp)
-                    cost, pos = optimizer.optimize(fx.rastrigin, iters=int(n))
-                elif variable == 'Num_Particles':
-                    # optimizes function on different numver of particles
-                    optimizer = ps.single.GlobalBestPSO(n_particles=int(
-                        n), dimensions=2, options=options, bounds=bounds, velocity_clamp=velocity_clamp)
-                    cost, pos = optimizer.optimize(fx.rastrigin, iters=50)
-                elif variable == 'Num_dimensions':
-                    # optimizes function on different numver of dimensions
-                    max_bound = 5.12 * np.ones(int(n))
-                    min_bound = -max_bound
-                    bounds = (min_bound, max_bound)
-                    optimizer = ps.single.GlobalBestPSO(n_particles=20, dimensions=int(
-                        n), options=options, bounds=bounds, velocity_clamp=velocity_clamp)
-                    cost, pos = optimizer.optimize(fx.rastrigin, iters=50)
-            print()
-    csvfile.close()
+# for validation on basis of varying number of particles RANGE [10,500]
+# for K in range (10, 510, 10):
+#     runtime=[]
+#     for i in range (0,10):
+#         start = time.time()
+#         max_bound = 5.12 * np.ones(2)
+#         # print(max_bound)
+#         min_bound = - max_bound
+#         # print(min_bound)
+#         bounds = (min_bound, max_bound)
+#         velocity_clamp = (0, 15)
+#         options = {'c1': 0.5, 'c2': 0.3, 'w': 0.9}
+#         optimizer = ps.single.GlobalBestPSO(n_particles=K, dimensions=2, options=options, bounds=bounds, velocity_clamp=velocity_clamp)
+#         optimizer.optimize(fx.rastrigin, 50)
+#         end=time.time()
+#         runtime.append(end-start)
+#         # print(runtime)
+#     for j in runtime:
+#         print(int(j*1000000))
 
 
-LoadData("TestValues.csv")
-# Perform optimization
+# # for validation on basis of varying number of dimensions RANGE [2,25]
+# for K in range (2, 26):
+#     runtime=[]
+#     for i in range (0,10):
+#         start = time.time()
+#         max_bound = 5.12 * np.ones(K)
+#         # print(max_bound)
+#         min_bound = - max_bound
+#         # print(min_bound)
+#         bounds = (min_bound, max_bound)
+#         velocity_clamp = (0, 15)
+#         options = {'c1': 0.5, 'c2': 0.3, 'w': 0.9}
+#         optimizer = ps.single.GlobalBestPSO(n_particles=20, dimensions=K, options=options, bounds=bounds, velocity_clamp=velocity_clamp)
+#         optimizer.optimize(fx.rastrigin, 50)
+#         end=time.time()
+#         runtime.append(end-start)
+#         # print(runtime)
+#     for j in runtime:
+#         print(int(j*1000000))
+
+
+# for validation on basis of varying number of iters RANGE [100,3000]
+# for K in range (100, 3100, 100):
+#     runtime=[]
+#     for i in range (0,10):
+#         start = time.time()
+#         max_bound = 5.12 * np.ones(K)
+#         # print(max_bound)
+#         min_bound = - max_bound
+#         # print(min_bound)
+#         bounds = (min_bound, max_bound)
+#         velocity_clamp = (0, 15)
+#         options = {'c1': 0.5, 'c2': 0.3, 'w': 0.9}
+#         optimizer = ps.single.GlobalBestPSO(n_particles=20, dimensions=K, options=options, bounds=bounds, velocity_clamp=velocity_clamp)
+#         optimizer.optimize(fx.rastrigin, 50)
+#         end=time.time()
+#         runtime.append(end-start)
+#         # print(runtime)
+#     for j in runtime:
+#         print(int(j*1000000))
